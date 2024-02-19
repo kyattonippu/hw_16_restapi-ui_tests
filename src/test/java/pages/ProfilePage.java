576eeb68db1dd5b1b0196146a82b1be8920e9fea
kyattonippu <kyattonippu@gmail.com>
2024-02-19 08:55:41 +0800

    2

--- src/test/java/pages/ProfilePage.java
+++ src/test/java/pages/ProfilePage.java
@@ -5,7 +5,6 @@ import com.codeborne.selenide.SelenideElement;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 import static data.ApiEndpoints.PROFILE;
@@ -24,7 +23,7 @@ public class ProfilePage {
 
     public void clickOnCookieConsentIfDisplayed() {
         if (bannerRoot.isDisplayed()) {
-            bannerRoot.$("fc-button fc-cta-consent fc-primary-button").click();
+            bannerRoot.$("button.fc-cta-consent").click();
         }
     }
 
